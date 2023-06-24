ad8c13234caf0fd84c45fbfc698c9200021fcf05
Surya <suryapratapbbr21@gmail.com>
2023-06-24 10:05:06 +0530

    Changes

--- src/Components/Header/Components/Topper/index.js
+++ src/Components/Header/Components/Topper/index.js
@@ -9,13 +9,13 @@ const Topper = () => {
       <Container>
         <div className={styles["topper__main-number"]}>
           <i className={`fas fa-phone-alt ${styles["topper__main-icon"]}`}></i>+
-          1235 2355 98
+          +91 99889-05110
         </div>
         <div className={styles["topper__main-email"]}>
           <i
             className={`fas fa-paper-plane ${styles["topper__main-icon"]}`}
           ></i>
-          test-email@email.com
+          shivazhandloom@gmail.com
         </div>
         <div className={styles["topper__main-delivery"]}>
           3-5 Business days delivery
